899496025eb42f3248ba88c830ad744ec4aa3c01
dinmoy <imhyojin614@gmail.com>
2023-11-02 16:46:26 +0900

    [Feat] Response 클래스 생성

--- app/src/main/java/com/example/retrofitstudy/api/APIService.kt
+++ app/src/main/java/com/example/retrofitstudy/api/APIService.kt
@@ -5,8 +5,8 @@ import retrofit2.http.GET
 import java.util.Date
 
 data class Post(val id: Int, var title: String, var author: String, val createdAt: Date, var content : String)
-data class  AllPostReaponse (val result: List<>)
+data class  AllPostReaponse (val result: List<Post>)
 interface APIService {
     @GET("/posts")
-    fun getPosts() : Call<>
+    fun getPosts() : Call<AllPostReaponse>
 }
