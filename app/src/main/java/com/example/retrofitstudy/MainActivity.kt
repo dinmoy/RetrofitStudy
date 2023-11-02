30886827d13db306431644365a205dab2964ae73
dinmoy <imhyojin614@gmail.com>
2023-11-02 16:48:59 +0900

    [Feat] Retrofit 객체 생성

--- app/src/main/java/com/example/retrofitstudy/MainActivity.kt
+++ app/src/main/java/com/example/retrofitstudy/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.retrofitstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit= Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:3000")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
