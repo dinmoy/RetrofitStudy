0f9296c0dae454cdb8781181b27831f5aa99db34
dinmoy <imhyojin614@gmail.com>
2023-11-02 16:45:21 +0900

    [Test] 테스트 커밋

--- app/src/main/java/com/example/retrofitstudy/api/APIService.kt
+++ app/src/main/java/com/example/retrofitstudy/api/APIService.kt
@@ -1,7 +1,12 @@
 package com.example.retrofitstudy.api
 
+import retrofit2.Call
 import retrofit2.http.GET
+import java.util.Date
 
+data class Post(val id: Int, var title: String, var author: String, val createdAt: Date, var content : String)
+data class  AllPostReaponse (val result: List<>)
 interface APIService {
     @GET("/posts")
+    fun getPosts() : Call<>
 }
