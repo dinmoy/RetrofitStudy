1a6d0918392f4c2f9552e72a9ea7573433161e7f
dinmoy <imhyojin614@gmail.com>
2023-11-02 16:51:51 +0900

    [Feat] 인터페이스 객체 생성

--- app/src/main/java/com/example/retrofitstudy/MainActivity.kt
+++ app/src/main/java/com/example/retrofitstudy/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.retrofitstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.retrofitstudy.api.APIService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var apiService: APIService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +16,7 @@ class MainActivity : AppCompatActivity() {
             .baseUrl("http://10.0.2.2:3000")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+
+        apiService=retrofit.create(APIService::class.java)
     }
 }
