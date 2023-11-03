5950ed6ff1895a38ce5d899516e0f61ffab07cb2
dinmoy <imhyojin614@gmail.com>
2023-11-03 15:38:27 +0900

    [Feat] delete Post 구현

--- app/src/main/java/com/example/retrofitstudy/MainActivity.kt
+++ app/src/main/java/com/example/retrofitstudy/MainActivity.kt
@@ -2,7 +2,16 @@ package com.example.retrofitstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.retrofitstudy.api.APIService
+import com.example.retrofitstudy.api.AllPostReaponse
+import com.example.retrofitstudy.api.Post
+import com.example.retrofitstudy.api.PostCreateRequest
+import com.example.retrofitstudy.api.PostResponse
+import com.example.retrofitstudy.api.StringResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -18,5 +27,56 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         apiService=retrofit.create(APIService::class.java)
+        //getPosts()
+        getPost(1)
+        deletePost(1)
     }
+//    fun getPost(){
+//        val call=apiService.getPosts()
+//        call.enqueue(object: Callback<AllPostReaponse>{
+//            override fun onResponse( call: Call<AllPostReaponse>, response: Response<AllPostReaponse>) {
+//                val data: AllPostReaponse?=response.body()
+//                data?.let{
+//                    Log.d("mytag",it.result.toString())
+//                }
+//            }
+//            override fun onFailure(call: Call<AllPostReaponse>, t: Throwable) {
+//            }
+//
+//        })
+//    }
+    fun getPost(id :Int){
+        val call=apiService.getPost(id)
+        call.enqueue(object: Callback<PostResponse>{
+            override fun onResponse(call: Call<PostResponse>, response: Response<PostResponse>) {
+                if(response.isSuccessful){
+                    val data: PostResponse?=response.body()
+                    data?.let{
+                        Log.d("mytag",it.result.toString())
+                    }
+                }else{
+
+                }
+            }
+
+            override fun onFailure(call: Call<PostResponse>, t: Throwable) {
+            }
+
+        })
+    }
+
+    fun deletePost(id: Int){
+        //호출준비
+        val call=apiService.deletePost(id)
+        //호출(익명 메서드)
+        call.enqueue(object: Callback<StringResponse>{
+            override fun onResponse(call: Call<StringResponse>,esponse: Response<StringResponse>
+            ) { }
+
+            override fun onFailure(call: Call<StringResponse>, t: Throwable) { }
+
+        })
+
+    }
+
 }
--- app/src/main/java/com/example/retrofitstudy/api/APIService.kt
+++ app/src/main/java/com/example/retrofitstudy/api/APIService.kt
@@ -1,12 +1,31 @@
 package com.example.retrofitstudy.api
 
 import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 import java.util.Date
 
 data class Post(val id: Int, var title: String, var author: String, val createdAt: Date, var content : String)
 data class  AllPostReaponse (val result: List<Post>)
+data class PostResponse(val result: Post)
+data class PostCreateRequest(val title: String, var author: String, var content : String)
+data class StringResponse(val result: String)
 interface APIService {
     @GET("/posts")
     fun getPosts() : Call<AllPostReaponse>
+
+    @GET("/posts/{id}")
+    fun getPost(@Path("id") id : Int) : Call<PostResponse>
+
+    @POST("/posts")
+    fun createPost(@Body request : PostCreateRequest) : Call<StringResponse>
+
+    //DELETE 요청 보내는deletePost 메서드 정의
+
+    @DELETE("/posts/{id}")
+    fun deletePost(@Path("id") id : Int) : Call<StringResponse>
+
 }
